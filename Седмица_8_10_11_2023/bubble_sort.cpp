5d7d4f1683b0816e5a9d1b31f696e8b3e9a3a4b2
Dimitar Shtregarski <58916033+DimitarSht@users.noreply.github.com>
2023-11-19 23:36:11 +0200

    Update bubble_sort.cpp

--- Седмица_8_10_11_2023/bubble_sort.cpp
+++ Седмица_8_10_11_2023/bubble_sort.cpp
@@ -3,22 +3,22 @@ using namespace std;
 int main()
 {
   int N = 7, tmp;
-	int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+  int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
 
   // Метод на мехурчето (bubble sort) за сортиране на масива arr
-  for (i = 0; i < N - 1; i++) {
-		for (j = 0; j < N - i - 1; j++) {
-			if (arr[j] > arr[j + 1]) {
-				tmp = arr[j+1];
-				arr[j+1] = arr[j];
-				arr[j] = tmp;
-			}
+  for (int i = 0; i < N - 1; i++) {
+	for (int j = 0; j < N - i - 1; j++) {
+		if (arr[j] > arr[j + 1]) {
+			tmp = arr[j+1];
+			arr[j+1] = arr[j];
+			arr[j] = tmp;
 		}
 	}
+   }
 
-	cout << "Sorted array: \n";
-	for (int i = 0; i < N; i++) {
-    cout << " " << arr[i];
-  }
-	return 0;
+   cout << "Sorted array: \n";
+   for (int i = 0; i < N; i++) {
+	cout << " " << arr[i];
+   }
+   return 0;
 }
