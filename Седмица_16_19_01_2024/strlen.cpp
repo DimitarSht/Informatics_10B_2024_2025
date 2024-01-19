c2110e58130ed6e32d15b89c005de2d2a43b570d
Dimitar Shtregarski <58916033+DimitarSht@users.noreply.github.com>
2024-01-19 07:08:14 +0200

    Някои функции от <cstring>

---
+++ Седмица_16_19_01_2024/strlen.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    // Define a char aaray
+    char str[] = "Hello Students!";
+ 
+    // getting length of str using strlen()
+    int length = strlen(str);
+    cout<<"Length of the string is: "<<length;
+
+    return 0;
+}
